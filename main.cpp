b82f1c18865489306809c99213441c297188707a
Michael Gosling <contact@michaelgosling.me>
2019-02-18 14:13:34 -0400

    Add function to generated unsorted array. Prompt for array size in main.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
 #include "SortingAlgos.h"
 
+int* createUnsortedArray(int length) {
+    int array[length];
+    time_t t;
+    srand((unsigned)time(&t));
+    for (auto i = 0; i < length; i++)
+        array[i] = rand() % 32767;
+
+    return array;
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    SortingAlgos sorts;
+    int arraySize;
+    std::cout << "Enter an array size: ";
+    std::cin >> arraySize;
+
+    int* array = createUnsortedArray(arraySize);
+
+
     return 0;
 }
